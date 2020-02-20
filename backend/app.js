e77d0dc1cb2cf9d4ddceeb711c637ab54b20e253
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-02-20 13:51:10 -0300

    Fetching Initial Posts

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware')
-    next();
-});
-
-
-app.use((req, res, next) => {
-    res.send('Hello from express!')
+app.use('/api/posts' , (req, res, next) => {
+    const posts = [
+        {
+            id: 'g43f2f332rf', 
+            title: 'First server-side post', 
+            content: 'This is coming from the Server'
+        },
+        {
+            id: '325f432f6', 
+            title: 'Second server-side post', 
+            content: 'This is coming from the Server'
+        },
+    ];
+    
+    res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
