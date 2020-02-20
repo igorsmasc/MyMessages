05db7ce069f25b2202165ede13d90794dedabd53
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-02-20 15:42:46 -0300

    Adding the POST Backend Point

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/api/posts' , (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added successfully'
+    });
+    next();
+});
+
+app.get('/api/posts', (req, res, next) => {
     const posts = [
         {
-            id: 'g43f2f332rf', 
-            title: 'First server-side post', 
+            id: 'g43f2f332rf',
+            title: 'First server-side post',
             content: 'This is coming from the Server'
         },
         {
-            id: '325f432f6', 
-            title: 'Second server-side post', 
+            id: '325f432f6',
+            title: 'Second server-side post',
             content: 'This is coming from the Server'
         },
     ];
-    
+
     res.status(200).json({
-        message: 'Posts fetched succesfully!',
+        message: 'Posts fetched successfully!',
         posts: posts
     });
 });
