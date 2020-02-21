8288adc547469fc720d85f0a644937c935104dea
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-02-21 15:04:43 -0300

    Edit Feature

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://user:password@cluster0-lkefb.mongodb.net/my-messages?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://igor:192022bc@cluster0-lkefb.mongodb.net/my-messages?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to the database')
     })
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
         'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader(
         'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS');
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 });
 
@@ -41,6 +41,21 @@ app.post("/api/posts", (req, res, next) => {
     });
 });
 
+app.put("/api/posts/:id", (req, res, next) => {
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content,
+    })
+
+    console.log(req.body)
+
+    Post.updateOne({ _id: req.params.id }, post).then(result => {
+        console.log(result);
+        res.status(200).json({ message: 'Update successful!' })
+    })
+});
+
 app.get('/api/posts', (req, res, next) => {
     Post.find()
         .then(documents => {
@@ -52,7 +67,7 @@ app.get('/api/posts', (req, res, next) => {
 });
 
 app.delete('/api/posts/:id', (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id})
+    Post.deleteOne({ _id: req.params.id })
         .then(result => {
             console.log(result);
         })
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -5,16 +5,16 @@ import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostsService {
     private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     getPosts() {
         this.http
-            .get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+            .get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
             .pipe(map((postData) => {
                 return postData.posts.map(post => {
                     return {
@@ -34,16 +34,26 @@ export class PostsService {
         return this.postsUpdated.asObservable();
     }
 
+    getPost(id: String) {
+        return { ...this.posts.find(p => p.id === id) };
+    }
+
     addPost(title: string, content: string) {
         const post: Post = { id: null, title: title, content: content };
-        this.http.post<{message:string, postId: string}>('http://localhost:3000/api/posts', post)
+        this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
             .subscribe((responseData) => {
                 const id = responseData.postId;
                 post.id = id;
                 this.posts.push(post);
                 this.postsUpdated.next([...this.posts]);
             });
-        
+
+    }
+
+    updatePost(id: string, title: string, content: string) {
+        const post: Post = { id: id, title: title, content: content };
+        this.http.put('http://localhost:3000/api/posts/' + id, post)
+        .subscribe(response => console.log(response));
     }
 
     deletePost(postId: string) {
