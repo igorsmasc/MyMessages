565549d4d4aff332573dacbe8ba93fa9a7745aae
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-03-05 13:20:37 -0300

    Sending the Token to the Frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,13 +7,13 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => {
     const user = new User({
       email: req.body.email,
       password: hash
     });
-    user
-      .save()
+    user.save()
       .then(result => {
         res.status(201).json({
           message: "User created",
@@ -29,6 +29,9 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post("/login", (req, res, next) => {
+
+  let fetchedUser;
+
   User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
@@ -36,7 +39,7 @@ router.post("/login", (req, res, next) => {
           message: "Auth failed"
         });
       }
-
+      fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
@@ -47,10 +50,15 @@ router.post("/login", (req, res, next) => {
       }
 
       const token = jwt.sign(
-        { email: user.email, userId: user._id },
+        { email: fetchedUser.email, userId: fetchedUser._id },
         "secret_this_should_be_longer",
         { expiresIn: "1h" }
       );
+
+      res.status(200).json({
+        token: token
+      });
+
     })
     .catch(err => {
       return res.status(401).json({
