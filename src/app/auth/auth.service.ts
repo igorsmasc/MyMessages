2fd01453f992a2f831ceff22bb634332dfb649ef
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-03-06 15:37:23 -0300

    Connecting the Logout Button to the Authentication Status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -46,4 +46,11 @@ export class AuthService {
         }
       });
   }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+  }
+
 }
