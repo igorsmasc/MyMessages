efddb18ffaab07b6c50a66d5202f8b97ad094a35
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-03-06 15:34:00 -0300

    Improving the UI Messages to Reflect the Authentication Status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 
-import { AuthData } from './auth-data.model';
-import { Subject } from 'rxjs';
+import { AuthData } from "./auth-data.model";
+import { Subject } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+  private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
 
@@ -15,25 +16,34 @@ export class AuthService {
     return this.token;
   }
 
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+
   getAuthStatusListener() {
     return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
-    const authData: AuthData = {email: email, password: password};
-    this.http.post("http://localhost:3000/api/user/signup", authData)
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post("http://localhost:3000/api/user/signup", authData)
       .subscribe(response => {
-        console.log(response)
-      })
+        console.log(response);
+      });
   }
 
   login(email: string, password: string) {
-    const authData: AuthData = {email: email, password: password}
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post<{ token: string }>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
-        this.authStatusListener.next(true);
-      })
+        if (token) {
+          this.isAuthenticated = true;
+          this.authStatusListener.next(true);
+        }
+      });
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,7 +3,8 @@ import { Subscription } from "rxjs";
 
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
-import { PageEvent } from '@angular/material/paginator';
+import { PageEvent } from "@angular/material/paginator";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
   selector: "app-post-list",
@@ -17,19 +18,32 @@ export class PostListComponent implements OnInit, OnDestroy {
   postsPerPage = 3;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
+  userIsAuthenticated = false;
   private postsSub: Subscription;
+  private authStatusSubs: Subscription;
 
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.isLoading = true;
     this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService
       .getPostUpdateListener()
-      .subscribe((postData: {posts: Post[], postCount: number}) => {
+      .subscribe((postData: { posts: Post[]; postCount: number }) => {
         this.isLoading = false;
         this.posts = postData.posts;
         this.totalPosts = postData.postCount;
+      });
+
+    this.userIsAuthenticated = this.authService.getIsAuth();
+
+    this.authStatusSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
       });
   }
 
@@ -48,5 +62,6 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
+    this.authStatusSubs.unsubscribe();
   }
 }
