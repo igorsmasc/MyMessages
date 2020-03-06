ad38ce65ba77e15ec32187402fac63b04eab5567
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-03-06 15:22:34 -0300

    Improving the UI Header to Reflect the Authentication Status

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -11,7 +11,10 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<{ posts: Post[]; postCount: number }>();
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) {}
 
   getPosts(postsPerPage: number, currentPage: number) {
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
